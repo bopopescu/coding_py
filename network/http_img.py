15c876896c47753443d7ff971814c63df1355c1b
leung1024 <cljun8@hotmail.com>
2015-11-04 20:20:41 +0800

    add http_img

--- network/http_img.py
+++ network/http_img.py
@@ -4,9 +4,13 @@
 from daemon import Daemon
 import socket
 import time
+from multiprocessing import Pool
+import Queue
+import threading
 
 html = """HTTP/1.1 200 OK\r\nContent-Type: image/jpeg\r\nConnection: close\r\nContent-Length: """
 html404 = """HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nContent-Length: 13\r\n\r\n<h1>404 </h1>"""
+queue = Queue.Queue(20)
 
 class agentD(Daemon):
     def run(self):
@@ -17,18 +21,36 @@ class agentD(Daemon):
         while True:
             conn, addr = listen_fd.accept()
             print "coming", conn, addr
-            read_data = conn.recv(1000)
-
+            raw = {
+                'conn':conn,
+                'addr':addr
+                }
+            print raw
             try:
-                pic_name = read_data.split(" ")[1][1:]
-                print pic_name
+                print queue.qsize()
+                queue.put(raw)
+                print queue.get()
+            except Exception as e:
+                print e
+
+
+def worker(name):
+    while True:
+        if queue.qsize() > 0:
+            raw = queue.get()
+            conn = raw['raw']
+            addr = raw['addr']
+            read_data = conn.recv(1000)
+            pic_name = read_data.split(" ")[1][1:]
+            print pic_name
+            try:
                 with file(pic_name) as f:
-                    pic_content = f.read()
+                    pic_conntent = f.read()
                     length = len(pic_content)
                     http_resp = html
-                    http_resp += "%d\r\n\r\n" % length
+                    http_resp += "%d\r\n\r\b" % length
                     print html_resp
-                    html_resp += pic_content
+                    html_resp += pic_conntent
             except:
                 print "404"
                 html_resp = html404
@@ -38,7 +60,24 @@ class agentD(Daemon):
                 print "send: ", sent_cnt
                 html_resp = html[sent_cnt:]
             conn.close()
-
-if __name__ == '__main__':
+def init_agent(name):
+    print name
     agented = agentD(pidfile = "agentd.pid", stdout = "agentd.log", stderr = "agentd.log")
     agented.run()
+
+if __name__ == '__main__':
+    #agented = agentD(pidfile = "agentd.pid", stdout = "agentd.log", stderr = "agentd.log")
+    #agented.run()
+    #queue = Queue()
+    max_processes = 2
+    pool = Pool(processes = 2)
+    task_list = list(range(max_processes))
+    for i in task_list:
+        print queue.qsize()
+        if not i:
+            pool.apply_async(init_agent, (i,))
+        else:
+            pool.apply_async(worker, (i,))
+
+    pool.close()
+    pool.join()
