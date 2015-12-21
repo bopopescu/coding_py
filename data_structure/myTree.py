4a01533e93459dfb86e724bf2780660acd56c257
lujunliang <rcleung526@gmail.com>
2015-12-21 17:55:39 +0800

    fix tree

--- data_structure/myTree.py
+++ data_structure/myTree.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #coding=utf-8
 
 class ChildTree:
@@ -13,11 +13,16 @@ class ChildTree:
     def getRightChild(self):
         return self.right_child
 
-    def setLeftChild(self, new_tree):
-        self.left_child = new_tree
+    def getValue(self):
+        return self.data
 
-    def setRightChild(self, new_tree):
-        self.right_child = new_tree
+    def setLeftChild(self):
+        self.left_child = ChildTree(None)
+        return self.left_child
+
+    def setRightChild(self):
+        self.right_child = ChildTree(None)
+        return self.right_child
 
     def setValue(self, val):
         self.data = val
@@ -26,19 +31,39 @@ class BinaryTree:
     def __init__(self, root):
         self.root = ChildTree(root)
 
+    def createTree(self, node):
+        if node == self.root:
+            print("Now set root value,")
+        val = input("please input node value:")
+        if val != '0':
+            print('=============================')
+            node.setValue(val)
+            print("Now insert left node,")
+            self.createTree(node.setLeftChild())
+            print("Now insert right node,")
+            self.createTree(node.setRightChild())
+        else:
+            print("/------skip this node------/")
+
+
     def insert_left(self, new_node):
-        if self.left_child == None:
-            self.left_child == ChildTree(new_node)
+        if self.root.left_child == None:
+            self.root.left_child = ChildTree(new_node)
         else:
             tmp = ChildTree(new_node)
             tmp.setLeftChild(self.root.getLeftChild())
             self.root.setLeftChild(tmp)
 
     def insert_right(self, new_node):
-        if self.right_child == None:
-            self.right_child == ChildTree(new_node)
+        if self.root.right_child == None:
+            self.root.right_child = ChildTree(new_node)
         else:
             tmp = ChildTree(new_node)
             tmp.setRightChild(self.root.getRightChild())
             self.root.setRightChild(tmp)
 
+if __name__ == '__main__':
+    bt = BinaryTree(24)
+    bt.createTree(bt.root)
+    print(bt.root.getLeftChild().getValue())
+    print(bt.root.getRightChild().getValue())
