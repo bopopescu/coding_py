175c05eb4916876b061870b19b0ad7c39f65c0b1
lujunliang <rcleung526@gmail.com>
2015-12-22 18:05:43 +0800

    add postorder and inorder

--- data_structure/BinaryTraver.py
+++ data_structure/BinaryTraver.py
@@ -1,29 +1,80 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #coding=utf-8
 
 from myTree import BinaryTree
+import pdb
 
 def preorder_traverse(binary_tree):
-    reslut = []
+    result = []
     parent_stack = []
     if binary_tree.root:
         node = binary_tree.root
         while node:
-            result.append(node.getValue())
+            val = node.getValue()
+            if val:
+                result.append(val)
             parent_stack.append(node)
             node = node.getLeftChild() 
             if not node:
                 while not node:
+                    if len(parent_stack) != 0:
+                        node = parent_stack.pop()
+                        node = node.getRightChild()
+                    else:
+                        break
+
+    return result
+
+def inorder_traverse(binary_tree):
+    result = []
+    parent_stack = []
+    if binary_tree.root:
+        node = binary_tree.root
+        while node:
+            parent_stack.append(node)
+            node = node.getLeftChild()
+            while not node:
+                if len(parent_stack) != 0:
                     node = parent_stack.pop()
+                    val = node.getValue()
+                    if val:
+                        result.append(val)
                     node = node.getRightChild()
+                else:
+                    break
+    return result
 
-    return reslut
+def postorder_traverse(binary_tree):
+    result = []
+    parent_stack = []
+    if binary_tree.root:
+        node = binary_tree.root
+        while node:
+            if node.data:
+                parent_stack.append(node)
+            node = node.getLeftChild()
+            pdb.set_trace()
+            while not node:
+                if len(parent_stack) != 0:
+                    node = parent_stack.pop()
+                    if not node.getRightChild():
+                        val = node.getValue()
+                        if val:
+                            reslut.append(val)
+                    else:
+                        node = node.getRightChild()
+                else:
+                    break
+    return result
 
+                    
 
 bt = BinaryTree(24)
-bt.createTree()
-#print(bt.root.getLeftChild())
-#bt.root.getLeftChild().insert_left(4)
-#bt.root.getLeftChild().insert_right(43)
-#bt.insert_right(27)
-#bt.root.getRightChild().insert_left(87)
+bt.createTree(bt.root)
+print("先序遍历")
+print(preorder_traverse(bt))
+print("中序遍历")
+print(inorder_traverse(bt))
+print("后序遍历")
+print(postorder_traverse(bt))
+
