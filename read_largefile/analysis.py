c5f6dfcdc73c8909a82d2366940edbd375f3559f
lujunliang <rcleung526@gmail.com>
2016-01-28 12:54:19 +0800

    add

--- read_largefile/analysis.py
+++ read_largefile/analysis.py
@@ -24,6 +24,7 @@ def choose_data(line, base_filter):
     date_filter = '\d*-\d*-\d*'
     time_filter = '\d*:\d*:\d*'
     dict_filter = '{.*}$'
+    batch_filter = "'batch': \d*"
     valid_line = re.search(base_filter, line)
     if valid_line:
         result = {
@@ -31,28 +32,32 @@ def choose_data(line, base_filter):
             'log_time': re.search(time_filter, line).group(),
             'data': eval(re.search(dict_filter, line).group())
             }
+        result.update(eval("{%s}" % re.search(batch_filter, line).group()))
         return result
 
 def analysis_data(p_data, key_filter, value_filter):
-    order_list = []
     result = []
     for line in p_data:
+        order_list = []
         for order in line['data'].values():
             if order[key_filter] == value_filter:
                 order_list.append(order)
         result.append({
                 'log_date': line['log_date'],
                 'log_time': line['log_time'],
+                'batch': line['batch'],
                 'data': order_list
             }
         )
     return result
 
 if __name__ == '__main__':
-    key_filter = 'sort_id'
-    value_filter = 246
+    key_filter = 'detail_id'
+    value_filter = 'L'
     if len(sys.argv) > 1 and re.search('.log$', sys.argv[1]):
         file_name = sys.argv[1]
-        pprint(process_file_data(file_name = file_name, key_filter=key_filter, value_filter=value_filter))
+        result = process_file_data(file_name = file_name, key_filter=key_filter, value_filter=value_filter)
+        pprint(result)
+        print(len(result))
     else:
         print("input file name")
